01389c1daf1c768095b33dfe39576d9037b9cd1c
kiteros <jules.eschbach@icloud.com>
2017-04-23 14:52:31 +0200

    bug

--- include/bdd.php
+++ include/bdd.php
@@ -1,19 +1,19 @@
 <?php
-/*try
+try
 {
   $bdd = new PDO('mysql:host=jeschbacplbdd.mysql.db;dbname=jeschbacplbdd;charset=utf8', 'jeschbacplbdd', 'Jules1234FTP');
 }
 catch(Exception $e)
 {
       die('Erreur : '.$e->getMessage());
-}*/
+}
 
-try
+/*try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=yape;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
-}
+}*/
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script>
-$.getJSON('http://api.wipmania.com/jsonp?callback=?', function (data) {
+$.getJSON('http://api.wipmania.com/jsonp?callback=true', function (data) {
   var contryCode = data.address.country_code;
   window.location.replace('mainpage.php?lang=' + contryCode);
 });
 </script>
+<p>Nothing haoen</p>
